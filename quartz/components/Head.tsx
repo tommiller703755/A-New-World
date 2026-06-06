418099352659020285ca83fddc694a05bc1d9c81
Thomas John Miller <tom_miller@DESKTOP-CCGROG6.localdomain>
2026-06-05 22:23:04 -0500

    Quartz sync: Jun 5, 2026, 10:23 PM

--- quartz/components/Head.tsx
+++ quartz/components/Head.tsx
@@ -4,7 +4,6 @@ import { CSSResourceToStyleElement, JSResourceToScriptElement } from "../util/re
 import { googleFontHref, googleFontSubsetHref } from "../util/theme"
 import { QuartzComponent, QuartzComponentConstructor, QuartzComponentProps } from "./types"
 import { unescapeHTML } from "../util/escape"
-import { CustomOgImagesEmitterName } from "../../.quartz/plugins"
 export default (() => {
   const Head: QuartzComponent = ({
     cfg,
